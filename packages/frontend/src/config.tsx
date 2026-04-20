ae15b2b93b549b78eb34fd9548ad8b13a4d1dc6e
serhiigrin4 <serhiigrin4@gmail.com>
2026-04-20 14:43:35 +0300

    fix: update GATEWAY_URL to use new API endpoint instead of localstack

--- packages/frontend/src/config.tsx
+++ packages/frontend/src/config.tsx
@@ -1,15 +1,8 @@
-const getOrigin = () => {
-    const origin = window.location.origin;
-    if (origin.indexOf('localhost') !== -1) {
-        return 'http://localhost:4566';
-    }
-    return origin;
-}
+// Беремо адресу API з GitHub Actions, або використовуємо пряме посилання як запасний варіант
+export const GATEWAY_URL = import.meta.env.VITE_API_URL || 'https://api.serhiigrin4-games.pp.ua';
 
-
-export const GATEWAY_URL = `${getOrigin()}/restapis/${import.meta.env.VITE_GATEWAY_ID}/prod/_user_request_/`;
-export const  MAX_FILE_SIZE = 500000;
-export const  FILES_BUCKET = import.meta.env.VITE_FILES_BUCKET;
+export const MAX_FILE_SIZE = 500000;
+export const FILES_BUCKET = import.meta.env.VITE_FILES_BUCKET;
 export const REGION = import.meta.env.VITE_REGION;
 export const IDENTITY_POOL_ID = import.meta.env.VITE_IDENTITY_POOL_ID;
-export const  BASE_URL =  import.meta.env.BASE_URL;
+export const BASE_URL = import.meta.env.BASE_URL;
